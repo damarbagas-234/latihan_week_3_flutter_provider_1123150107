90723567c141e131b38fec47339d9f52d7a4010a
damarbagas-234 <muhammaddamar107@gmail.com>
2026-02-27 19:55:26 +0700

    mengubah kode pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'core/routes/app_router.dart' as core_router;
+import 'core/routes/app_router.dart';
+import 'injection.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(buildApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: core_router.AppRouter.catalog,
-      routes: core_router.AppRouter.routes,
+      // Menggunakan konstanta route dari AppRouter di Core Layer
+      initialRoute: AppRouter.catalog,
+      routes: AppRouter.routes,
     );
   }
 }
