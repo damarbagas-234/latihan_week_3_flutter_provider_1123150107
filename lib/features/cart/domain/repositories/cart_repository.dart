26c31a9d396306ec60329c588a2420a0636e0120
damarbagas-234 <muhammaddamar107@gmail.com>
2026-02-28 20:40:52 +0700

    Tambahkan metode removeItem pada CartRepository

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -3,6 +3,7 @@ import '../entities/product.dart';
 abstract class CartRepository {
   List<Product> getCartItems();
   void addItem(Product product);
+  void removeItem(String productId);
   void removeAllItems();
   bool isItemInCart(String productId);
 }
